755e1bcb753d69098940b74d85a824cf7e5f3da2
SarahAlhindi <144829728+SarahAlhindi@users.noreply.github.com>
2025-12-29 16:14:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.getElementById("guestName").innerText = guest;
 // Google Apps Script Web App URL (NEW)
 // ==========================
 const WEB_APP_URL =
-  "https://script.google.com/macros/s/AKfycbxHrjWmeAHZhMYxyNuxZHFYkEnocHZapvIhdrRkCfjXH7quyLCLmxBvxt2Y77Gu6CzKqA/exec";
+  "https://script.google.com/macros/s/AKfycbxAgqFkj0lhmELBBmGXAoemkuivLVh8_t9ToIK9bRKZVCB8WgCXAGF8TAL4bkWzpc8aNQ/exec";
 
 // ==========================
 // Send RSVP (FORM DATA – no CORS issues)
@@ -41,3 +41,4 @@ document.getElementById("yesBtn")
 
 document.getElementById("noBtn")
   .addEventListener("click", () => sendRSVP(guest, "لن أتمكن"));
+
